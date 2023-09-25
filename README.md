7eb1b31750220f73972438e3ada1a1c39779d438
artemsites <info@artemsites.ru>
2023-09-25 23:03:38 +0400

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# [Оригинальная статья находится на сайте practical-web.ru](https://practical-web.ru/sborshchiki/kak-ustanovit-i-primenyat-sborshchik-frontend-prilozheniy-vite-js)
---
+++ assets/js/switcher.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+  try {
+
+    const switcherAll = document.querySelectorAll(".switcher");
+    switcherAll.forEach(function (switcher) {
+      switcher.addEventListener("click", function (e) {
+        if (switcher.classList.contains("_on")) {
+          switcher.classList.remove("_on");
+          switcher.classList.add("_off");
+        } else {
+          switcher.classList.remove("_off");
+          switcher.classList.add("_on");
+        }
+
+        // Установка значения checked в value
+        const checkbox = switcher.querySelector(".switcher__checkbox");
+        checkbox.value=checkbox.checked
+
+        // Сброс анимации сплющивания
+        const indicator = switcher.querySelector(".switcher__indicator");
+        indicator.style.animationName = "none";
+        requestAnimationFrame(function () {
+          indicator.style.animationName = "";
+        });
+      });
+    });
+
+  } catch (err) {
+    console.error("switcher.js: ", err);
+  } finally {
+  }
+});
